b27f1ec6e12f6580dcd4a7b4c2f7738d070c8e82
Baj <benjphilippe@hotmail.fr>
2018-04-09 19:53:49 +0200

    added bezier splines

---
+++ Assets/Scripts/Booster.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Booster : MonoBehaviour {
+	public BezierSolution.BezierWalkerWithSpeed bzWalker;
+	public string currentPhase = "";	// liftoff, landing
+	public bool deployLegs = false;
+	public float acceleration;
+	private void Update() {
+		Accelerate(acceleration);
+	}
+	public void Accelerate(float factor){
+		bzWalker.speed += bzWalker.speed * factor * Time.deltaTime;
+	}
+	public void Decelerate(float factor){
+		bzWalker.speed -= bzWalker.speed * factor;
+	}
+}
