9b06bf2a49f81931a3e41323ba5f46f424e75d1d
Baj <benjphilippe@hotmail.fr>
2018-04-09 12:42:12 +0200

    Added time controller
    time display, time multiplier and pause
    
    test UI

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public UIManager UIM;
+	public WebComManager WebCom;
+	public float timeValue = -10f;
+	public float minTimeValue, maxTimeValue;
+	public float timeMultiplier = 1f;
+	public bool isPlayMode = false;
+
+	// Use this for initialization
+	void Start () {
+		if(UIM == null){
+			Object.FindObjectOfType<UIManager>();
+		}
+		UIM.timeSlider.minValue = minTimeValue;
+		UIM.timeSlider.maxValue = maxTimeValue;
+		UIM.timeSlider.value = UIM.timeSlider.minValue;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isPlayMode){
+			timeValue += Time.deltaTime * timeMultiplier;
+			UIM.timeSlider.value = timeValue;
+		}else{
+			timeValue = UIM.timeSlider.value;
+		}
+	}
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+	//UI Components
+	public Text timeText, timeMultText;
+	public Slider timeSlider, timeMultSlider;
+
+	public GameManager GM;
+	private void Awake() {
+		GM = Object.FindObjectOfType<GameManager>();
+		if(GM == null){
+			Debug.Log("No GameManager found.");
+		}
+	}
+	private void Update() {
+		timeText.text = FormatTime(GM.timeValue);
+	}
+	public void PauseButton(){
+		GM.isPlayMode = !GM.isPlayMode;
+	}
+	public void ChangeTimeSpeed(){
+		GM.timeMultiplier = timeMultSlider.value;
+		timeMultText.text = GM.timeMultiplier.ToString() + "x";
+	}
+	public void PauseButton(bool b){
+		GM.isPlayMode = b;
+	}
+	public string FormatTime(float t){
+		int sec = (int)(Mathf.Abs(t) % 60);
+		int min = (int)(Mathf.Abs(t) / 60) % 60;
+		int ct = (int)(Mathf.Abs(t) * 100) % 100;
+		string s = "";
+		if(t<0){
+			s = "- ";
+		}
+		s += string.Format("{0:00}:{1:00}:{2:00}", min, sec, ct);
+		return s;
+	}
+}
